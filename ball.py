51e96bccbdd7de9f2f93fb90653f868c49f2fd79
rlawlcks <chankim0129@naver.com>
2025-06-19 20:53:42 +0900

    Add files via upload

--- ball.py
+++ ball.py
@@ -4,15 +4,17 @@ import random
 
 game = Ursina()
 
+# 게임 효과 사운드
 bgm = Audio('bgm.wav', loop=True, autoplay=False)
 coin_sound = Audio("coin_sound.mp3", autoplay=False)
 ending_sound = Audio("finish.mp3", autoplay=False)
 
+# 게임 환경 설정
 player = Entity(model="sphere", color=color.green, position=(0, 3, -400), scale=(5, 5, 5), collider="box")
 camera.z = -15
 camera.add_script(SmoothFollow(target=player, offset=(0, 5, -30)))
 road = Entity(model="plane", scale=(100, 10, 30000), color=color.black)
-rows = [-15, -10, -5, 0, 5, 10, 15]
+rows = [-15, -10, -5, 0, 5, 10, 15] # 장애물이 랜덤하게 나타남
 median_r = Entity(model='cube', collider='box', position=(25, 2, 0), scale=(5, 10, 30000), color=color.white)
 median_l = Entity(model='cube', collider='box', position=(-25, 2, 0), scale=(5, 10, 30000), color=color.white)
 score_board = Text(text="0", scale=5, x=-0.75, y=0.45)
@@ -25,6 +27,7 @@ coin_score = 0
 game_over = False
 restart_text = None
 
+# 장애물, 코인 생성 함수
 def generate_enemies_and_coins():
     for i in range(0, 30000, 100):
         enemy = Entity(
@@ -46,6 +49,7 @@ def generate_enemies_and_coins():
         )
         coins.append(coin)
 
+# 게임 종료
 def show_game_over(final_score):
     global game_over, restart_text
     game_over = True
@@ -58,8 +62,9 @@ def show_game_over(final_score):
         color=color.red,
         background=True
     )
-    ending_sound.play()
+    ending_sound.play() #게임이 종료가 되면 bgm도 중단됨
 
+# 게임 재시작할 경우
 def reset_game():
     global coin_score, game_over, restart_text
     player.position = Vec3(0, 3, -400)
@@ -106,6 +111,7 @@ def update():
         show_game_over(final_score)
         return
 
+    #코인을 먹으면 500점 추가
     for coin in coins[:]:
         if player.intersects(coin).hit:
             coin_score += 500
@@ -113,6 +119,7 @@ def update():
             destroy(coin)
             coins.remove(coin)
 
+    # 사용자의 위치가 -400에서 시작하기 때문에 400을 더했음
     base_score = player.z + 400
     total_score = base_score + coin_score
     score_board.text = str(int(total_score))
@@ -120,6 +127,7 @@ def update():
 generate_enemies_and_coins()
 bgm.play()
 
+# 배경
 window.fullscreen = True
 sky = Sky()
 game.run()
